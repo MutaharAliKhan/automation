358c3c3cd8ab49fa4f6602944d9d0f3cf23c494d
MutaharAliKhan <145846806+MutaharAliKhan@users.noreply.github.com>
2024-08-12 01:32:47 +0500

    Delete Data_Utility_Final directory

--- Data_Utility_Final/conftest.py
+++
@@ -1,9 +0,0 @@
-
-import pytest
-from database.database_connection import get_environment_config
-
-
-@pytest.fixture(scope='session')
-def base_url():
-    data = get_environment_config(r"D:\MyRecentProjects\Data_Utility\database\env_config.json")
-    return data.get("URL")
