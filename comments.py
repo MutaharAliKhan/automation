65f5e6b3aeaf5b5b5aab7a78b4112a295b67a921
MutaharAliKhan <145846806+MutaharAliKhan@users.noreply.github.com>
2024-08-13 18:14:40 +0500

    Update comments.py

--- comments.py
+++ comments.py
@@ -4,55 +4,26 @@ def extract_comments(script_content):
 
     in_multiline_comment = False
     multiline_comment_lines = []
+    start_delimiter = None
 
     for index, line in enumerate(lines):
         stripped_line = line.strip()
 
         if in_multiline_comment:
-            if stripped_line.endswith("'''") or stripped_line.endswith('"""'):
-                multiline_comment_lines.append(stripped_line)
-                comments[index + 1] = '\n'.join(multiline_comment_lines).strip()
-                in_multiline_comment = False
-                multiline_comment_lines = []
-            else:
-                multiline_comment_lines.append(stripped_line)
-        elif stripped_line.startswith("'''") or stripped_line.startswith('"""'):
-            if stripped_line.endswith("'''") or stripped_line.endswith('"""'):
-                comments[index + 1] = stripped_line
-            else:
-                multiline_comment_lines.append(stripped_line)
-                in_multiline_comment = True
-        elif stripped_line.startswith('#'):
-            comments[index + 1] = stripped_line
-
-    return comments
-
-
-
-def extract_comments(script_content):
-    comments = {}
-    lines = script_content.split('\n')
-
-    in_multiline_comment = False
-    multiline_comment_lines = []
-
-    for index, line in enumerate(lines):
-        stripped_line = line.strip()
-
-        if in_multiline_comment:
-            # Continue capturing multiline comment lines
             multiline_comment_lines.append(stripped_line)
-            if stripped_line.endswith("'''") or stripped_line.endswith('"""'):
+            if stripped_line.endswith(start_delimiter):
                 comments[index + 1] = '\n'.join(multiline_comment_lines).strip()
                 in_multiline_comment = False
                 multiline_comment_lines = []
         elif stripped_line.startswith("'''") or stripped_line.startswith('"""'):
             if stripped_line.endswith("'''") or stripped_line.endswith('"""'):
+                # Single-line multiline comment (starts and ends on the same line)
                 comments[index + 1] = stripped_line
             else:
-                # Start capturing a multiline comment
+                # Start of a multiline comment
                 multiline_comment_lines.append(stripped_line)
                 in_multiline_comment = True
+                start_delimiter = "'''" if stripped_line.startswith("'''") else '"""'
         elif stripped_line.startswith('#'):
             comments[index + 1] = stripped_line
 
