a2d02b13a7ae128d3f3559c46cc2a88b0187518d
MutaharAliKhan <145846806+MutaharAliKhan@users.noreply.github.com>
2024-08-13 18:04:46 +0500

    Update comments.py

--- comments.py
+++ comments.py
@@ -26,3 +26,34 @@ def extract_comments(script_content):
             comments[index + 1] = stripped_line
 
     return comments
+
+
+
+def extract_comments(script_content):
+    comments = {}
+    lines = script_content.split('\n')
+
+    in_multiline_comment = False
+    multiline_comment_lines = []
+
+    for index, line in enumerate(lines):
+        stripped_line = line.strip()
+
+        if in_multiline_comment:
+            # Continue capturing multiline comment lines
+            multiline_comment_lines.append(stripped_line)
+            if stripped_line.endswith("'''") or stripped_line.endswith('"""'):
+                comments[index + 1] = '\n'.join(multiline_comment_lines).strip()
+                in_multiline_comment = False
+                multiline_comment_lines = []
+        elif stripped_line.startswith("'''") or stripped_line.startswith('"""'):
+            if stripped_line.endswith("'''") or stripped_line.endswith('"""'):
+                comments[index + 1] = stripped_line
+            else:
+                # Start capturing a multiline comment
+                multiline_comment_lines.append(stripped_line)
+                in_multiline_comment = True
+        elif stripped_line.startswith('#'):
+            comments[index + 1] = stripped_line
+
+    return comments
